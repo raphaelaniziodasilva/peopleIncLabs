5c1fd2e23589d1c4b13582e8ccff2cb23a4a4800
Raphael Anizio da Silva <raphaelanizio01@gmail.com>
2024-03-29 19:08:40 -0300

    Adicionar arquivos de projeto.

---
+++ peopleIncLabs/Data/PersonContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
+using peopleIncLabs.Models;
+
+namespace peopleIncLabs.Data
+{
+    public class PersonContext : DbContext
+    {
+        public PersonContext(DbContextOptions<PersonContext> opts) : base(opts) { }
+
+        public DbSet<Person> Person { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>()
+                .HasIndex(p => p.Email)
+                .IsUnique();
+        }
+
+    }
+}
---
+++ peopleIncLabs/Models/Person.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace peopleIncLabs.Models
+{
+    public class Person
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        [MaxLength(50, ErrorMessage = "O tamanho do nome não pode exceder 50 caracteres")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "A idade é obrigatória")]
+        [Range(1, 150)]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "E-mail é obrigatório")]
+        [EmailAddress]
+        [MaxLength(150)]
+        public string Email { get; set; }
+
+    }
+}   
